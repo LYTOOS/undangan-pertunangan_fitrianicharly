ee759023ac4f5a83470ecbd3313db0b27455a082
LYTOOS <103782894+LYTOOS@users.noreply.github.com>
2026-01-18 21:15:59 +0700

    Add guest name handling and RSVP functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const params=new URLSearchParams(location.search);
+const guest=params.get("tamu")||"Tamu Undangan";
+document.getElementById("guestName").innerText=guest;
 
+function openInvite(){
+document.getElementById("cover").style.display="none";
+document.getElementById("music").play();
+}
+
+document.getElementById("rsvpBtn").onclick=()=>{
+saveRSVP(guest);
+window.open("https://wa.me/6282261467360");
+};
