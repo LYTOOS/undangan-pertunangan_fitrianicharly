69b8ef809e238a09bf6b88e639cbc2a275fe5da4
LYTOOS <103782894+LYTOOS@users.noreply.github.com>
2026-01-19 18:51:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-const p=new URLSearchParams(location.search);
-const guest=p.get("tamu")||"Tamu Undangan";
-document.getElementById("guestName").innerText=guest;
+// === AMBIL NAMA TAMU ===
+const params = new URLSearchParams(window.location.search);
+const guest =
+params.get("to") || params.get("tamu") || "Tamu Undangan";
 
+document.getElementById("guestName").innerText =
+decodeURIComponent(guest.replace(/\+/g," "));
+
+// === OPEN INVITATION ===
 function openInvite(){
 document.getElementById("cover").style.display="none";
 document.getElementById("music").play();
 }
 
+// === RSVP WHATSAPP ===
 document.getElementById("rsvpBtn").onclick=()=>{
 saveRSVP(guest);
-window.open("https://wa.me/6282261467360");
+window.open("https://wa.me/6282261467360","_blank");
 };
-
-<script>
-const params = new URLSearchParams(window.location.search);
-const guest = params.get("to");
-
-if(guest){
-  document.getElementById("guestName").innerText =
-  decodeURIComponent(guest.replace(/\+/g," "));
-}
-</script>
